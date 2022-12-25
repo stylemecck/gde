d8e22ae296d36b557fd576d60612db274c031a56
stylemecck <satyamkashyapoffical@gmail.com>
2022-12-25 08:57:04 +0530

    mail to form

---
+++ slide/subscribe.php
@@ -0,0 +1,17 @@
+<?php
+
+
+if($_POST["message"]) {
+
+
+mail("your@email.address", "Here is the subject line",
+
+
+$_POST["insert your message here"]. "From: satyamkumar001233@gmail.com");
+
+
+}
+
+
+?>
+
